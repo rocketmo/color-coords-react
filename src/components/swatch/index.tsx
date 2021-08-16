4a5666b46bebd334fba3d9afc8bc7707249fffe2
Nien Van <nienvan12@gmail.com>
2021-08-16 15:16:27 -0400

    Add random animation delay to swatch component

--- src/components/swatch/index.tsx
+++ src/components/swatch/index.tsx
@@ -1,19 +1,36 @@
+import React from "react";
 import "./swatch.css";
 import { TILE_SIZE, Color } from "../../constants";
 
 const SWATCH_SIZE = 20;
 const QUARTER_DIFF = (TILE_SIZE - SWATCH_SIZE) / 2;
 
-export default function SwatchComponent(props: { color: Color; row: number; col: number }) {
-    const colorClass = `swatch bg-${props.color}`;
-    const positionStyle = {
-        left: `${(props.col * TILE_SIZE) + QUARTER_DIFF}px`,
-        top: `${(props.row * TILE_SIZE) + QUARTER_DIFF}px`,
-    };
+interface SwatchComponentProps {
+    color: Color,
+    row: number,
+    col: number
+}
 
-    return (
-        <div className={colorClass} style={positionStyle}>
-            <div className="swatch-border"></div>
-        </div>
-    );
+export default class SwatchComponent extends React.Component<SwatchComponentProps> {
+    animationDelay: number;
+
+    constructor(props: SwatchComponentProps) {
+        super(props);
+        this.animationDelay = Math.random();
+    }
+
+    render(): JSX.Element {
+        const colorClass = `swatch bg-${this.props.color}`;
+        const swatchStyle = {
+            left: `${(this.props.col * TILE_SIZE) + QUARTER_DIFF}px`,
+            top: `${(this.props.row * TILE_SIZE) + QUARTER_DIFF}px`,
+            animationDelay: `-${this.animationDelay}s`
+        };
+
+        return (
+            <div className={colorClass} style={swatchStyle}>
+                <div className="swatch-border"></div>
+            </div>
+        );
+    }
 }
