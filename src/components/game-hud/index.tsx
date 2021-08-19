ecf2343d2c9e1949d8fd4c2fabfe46651bb29e90
Nien Van <nienvan12@gmail.com>
2021-08-19 03:47:58 -0400

    Add Game Menu

--- src/components/game-hud/index.tsx
+++ src/components/game-hud/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import { Turn as Hamburger } from "hamburger-react";
 import Solution from "../solution";
+import GameMenu from "../game-menu";
 import "./game-hud.css";
 import type Grid from "../../classes/grid";
 
@@ -11,11 +14,24 @@ interface GameHUDProps {
 }
 
 export default function GameHUD(props: GameHUDProps) {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className="game-hud">
             <div className="top-bar">
-                Level {props.level} | Moves: {props.movesTaken}
+                <div className="hamburger-btn">
+                    <Hamburger toggled={isMenuOpen} toggle={setMenuOpen} rounded
+                        size={24} label="Toggle menu" hideOutline={false}></Hamburger>
+                </div>
+                <div className="game-level">
+                    <span className="top-bar-text">Level {props.level}</span>
+                </div>
+                <div className="moves-taken">
+                    <span className="top-bar-text">Moves: {props.movesTaken}</span>
+                </div>
+                <div className="clearfix"></div>
             </div>
+            <GameMenu isOpen={isMenuOpen} setOpen={setMenuOpen} />
             <Solution grid={props.grid} playerRow={props.playerRow} playerCol={props.playerCol} />
         </div>
     );
---
+++ src/components/game-menu/index.tsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync, faThLarge, faQuestionCircle, faCog, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import "./game-menu.css";
+import React from "react";
+
+interface GameMenuProps {
+    isOpen: boolean,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function GameMenu(props: GameMenuProps) {
+    const openClass = props.isOpen ? "game-menu-open" : "game-menu-closed";
+    const overlayClass = `game-menu-overlay ${openClass}`;
+    const navClass = `game-menu-nav ${openClass}`;
+    const tabIndex = props.isOpen ? 0 : -1;
+    const handleClose = () => {
+        props.setOpen(false);
+    };
+
+    return (
+        <div className="game-menu">
+            <div className={overlayClass} onPointerUp={handleClose}></div>
+            <nav className={navClass}>
+                <a className="level-select-btn" href="#!" tabIndex={tabIndex}>
+                    <FontAwesomeIcon icon={faThLarge} />
+                    <span>Level Select</span>
+                </a>
+                <a className="restart-btn" href="#!" tabIndex={tabIndex}>
+                    <FontAwesomeIcon icon={faSync} />
+                    <span>Restart</span>
+                </a>
+                <a className="how-to-btn" href="#!" tabIndex={tabIndex}>
+                    <FontAwesomeIcon icon={faQuestionCircle} />
+                    <span>How to Play</span>
+                </a>
+                <a className="settings-btn" href="#!" tabIndex={tabIndex}>
+                    <FontAwesomeIcon icon={faCog} />
+                    <span>Settings</span>
+                </a>
+            </nav>
+        </div>
+    );
+}
